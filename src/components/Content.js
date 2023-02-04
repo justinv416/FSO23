3298404d39ec9bcb6d492f91914671af1d11ff40
justinv416 <vien_justin@hotmail.com>
2023-02-04 13:11:29 -0500

    completed ex. 1.2

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
+const Part = (props) => {
+   return (
+    <p>{props.name} {props.exercise}</p>
+   )
+} 
+
 const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
   return (
     <div>
-        <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part name={part1} exercise={exercises1}/>
+      <Part name={part2} exercise={exercises2}/>
+      <Part name={part3} exercise={exercises3}/>
     </div>
   )
 }
